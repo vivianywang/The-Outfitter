8c9bb417deccd3a8773229511fcc1960583fed38
vivianwang <vvccwang@gmail.com>
2024-06-30 16:58:52 -0400

    Initial Commit

---
+++ The Outfitter/The_OutfitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_OutfitterApp.swift
+//  The Outfitter
+//
+//  Created by Vivian Wang on 2024-06-30.
+//
+
+import SwiftUI
+
+@main
+struct The_OutfitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
